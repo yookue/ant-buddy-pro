ab7f776da9d64759903dc7969e8dbd40636288e7
yookue <yookue@outlook.com>
2023-08-14 00:12:27 +0800

    Update globalSettings.ts
    Fix HTTP context for gh-pages

--- config/globalSettings.ts
+++ config/globalSettings.ts
@@ -3,6 +3,6 @@ export default {
     organTrademark: 'Yookue',
     organUrl: 'https://yookue.com',
 
-    rootContext: '/yookue/ant-buddy-pro',
-    contextAssets: '/yookue/ant-buddy-pro/assets',
+    rootContext: '/ant-buddy-pro',
+    contextAssets: '/ant-buddy-pro/assets',
 }
