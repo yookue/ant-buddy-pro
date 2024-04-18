45c17c820fe06a405a9849df988d6e7102e449b3
yookue <yookue@outlook.com>
2024-04-18 18:14:27 +0800

    Update form/ChronoTuple

--- src/form/ChronoTuple/index.tsx
+++ src/form/ChronoTuple/index.tsx
@@ -104,7 +104,7 @@ export const ChronoTuple: React.FC<ChronoTupleProps> = (props?: ChronoTupleProps
                     label={props?.selectProps?.label ?? (formContext.vertical ? ' ' : '')}
                     {...restSelectProps}
                     proField={props?.proField}
-                    usePresetStyle={props?.selectProps?.usePresetStyle ?? 'addon'}
+                    usePresetStyle={(props?.selectProps?.usePresetStyle === undefined) ? 'addon' : false}
                 />
             </Input.Group>
         </div>
