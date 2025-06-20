c1d1d33256b8091b1bc3137b0d329d265d344deb
yookue <yookue@outlook.com>
2025-06-20 13:58:26 +0800

    Update FallbackImage & RefreshImage

--- src/field/FallbackImage/index.tsx
+++ src/field/FallbackImage/index.tsx
@@ -77,7 +77,7 @@ export const FallbackImage: React.FC<FallbackImageProps> = (props?: FallbackImag
     return (
         <RcImage
             className={classNames(clazzPrefix, `${clazzPrefix}-id-${fieldId}`, props?.className)}
-            src={imageSrc ?? ''}
+            src={`${imageSrc}`}
             {...omitProps}
         />
     );
