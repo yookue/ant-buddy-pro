cc9e5cc87d661322d681893503f4325d6991d4e9
yookue <yookue@outlook.com>
2026-05-07 15:11:17 +0800

    Add global styles define

---
+++ src/global.d.ts
@@ -0,0 +1,20 @@
+
+declare module '*.less' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: Record<string, string>;
+  export default content;
+}
